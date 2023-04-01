171c08e10891b8ee498bd390441c0b6187be232e
Sophia Marshment <themarshments@gmail.com>
2023-03-31 22:13:42 -0400

    conflict

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import './style.css';
 import './world';
 import "./entity/player";
-<<<<<<< HEAD
-=======
 import "./entity/enemy"
->>>>>>> b60121f16df8444aaa43e6bde15eaa6c8f9a16f1
 import "./entity/merchant"
