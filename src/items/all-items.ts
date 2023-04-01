fc5f40a54f178ed5a8a22149d97df319789d6417
Sophia Marshment <themarshments@gmail.com>
2023-04-01 04:45:46 -0400

    fix loading issue

--- src/items/all-items.ts
+++ src/items/all-items.ts
@@ -3,31 +3,31 @@ import Gun from "./gun";
 import Seed from "./seed";
 import Upgrade from "./upgrade";
 
-const seedBase = "assets/sproud-land/items/farm-items/seeds-";
+const seedBase = "assets/sproud-lands/Items/farm-items/seeds-";
 
 // new Gun(sprite, name, cost, damage, reload, fire rate, projectiles, max ammo, range, dps)
 const allGuns = [
-  new Gun(null, "revolver", 0, 20, 1, 2, 1, 6, 20, 40),
-  new Gun(null, "shotgun", 1, 10, 1.2, 6, 1, 20, 40, 60),
-  new Gun(null, "assaut rifle", 1, 10, 1, 2, 4, 2, 8, 80),
+  new Gun(Sprite.from(Texture.from(seedBase + "carrot.png")), "revolver", 0, 20, 1, 2, 1, 6, 20, 40),
+  new Gun(Sprite.from(Texture.from(seedBase + "carrot.png")), "shotgun", 1, 10, 1.2, 6, 1, 20, 40, 60),
+  new Gun(Sprite.from(Texture.from(seedBase + "carrot.png")), "assaut rifle", 1, 10, 1, 2, 4, 2, 8, 80),
 ]
 
 const allSeeds = [
-  new Seed(Sprite.from(Texture.from(seedBase + "carrot")), "carrot", 10),
-  new Seed(Sprite.from(Texture.from(seedBase + "wheat")), "wheat", 10),
-  new Seed(Sprite.from(Texture.from(seedBase + "parsnip")), "parsnip", 10),
-  new Seed(Sprite.from(Texture.from(seedBase + "cucumber")), "cucumber", 10),
+  new Seed(Sprite.from(Texture.from(seedBase + "carrot.png")), "carrot", 10),
+  new Seed(Sprite.from(Texture.from(seedBase + "wheat.png")), "wheat", 10),
+  new Seed(Sprite.from(Texture.from(seedBase + "parsnip.png")), "parsnip", 10),
+  new Seed(Sprite.from(Texture.from(seedBase + "cucumber.png")), "cucumber", 10),
 
-  new Seed(Sprite.from(Texture.from(seedBase + "tomato")), "tomato", 30),
-  new Seed(Sprite.from(Texture.from(seedBase + "corn")), "corn", 30),
-  new Seed(Sprite.from(Texture.from(seedBase + "kale")), "kale", 30),
+  new Seed(Sprite.from(Texture.from(seedBase + "tomato.png")), "tomato", 30),
+  new Seed(Sprite.from(Texture.from(seedBase + "corn.png")), "corn", 30),
+  new Seed(Sprite.from(Texture.from(seedBase + "kale.png")), "kale", 30),
 
-  new Seed(Sprite.from(Texture.from(seedBase + "cabbage")), "cabbage", 75),
-  new Seed(Sprite.from(Texture.from(seedBase + "cauliflower")), "cauliflower", 75),
+  new Seed(Sprite.from(Texture.from(seedBase + "cabbage.png")), "cabbage", 75),
+  new Seed(Sprite.from(Texture.from(seedBase + "cauliflower.png")), "cauliflower", 75),
 
-  new Seed(Sprite.from(Texture.from(seedBase + "pumpkin")), "pumpkin", 200),
+  new Seed(Sprite.from(Texture.from(seedBase + "pumpkin.png")), "pumpkin", 200),
 
-  new Seed(Sprite.from(Texture.from(seedBase + "star")), "star", 515),
+  new Seed(Sprite.from(Texture.from(seedBase + "star.png")), "star", 515),
 ]
 
 const gunUpgrades = [
--- src/items/item.ts
+++ src/items/item.ts
@@ -3,7 +3,7 @@ import { Sprite } from "pixi.js";
 export interface Item {
   id: string,
   name: string,
-  sprite: Sprite | null,
+  sprite: Sprite,
   cost: number,
   consumable: boolean,
   use(): void
